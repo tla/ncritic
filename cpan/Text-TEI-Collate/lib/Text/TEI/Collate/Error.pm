823c4198b786c72e167b55ed4308ad73c7459926
Tara L Andrews <tla@mit.edu>
2012-04-18 12:57:56 +0200

    stringify errors so they are useful

--- cpan/Text-TEI-Collate/lib/Text/TEI/Collate/Error.pm
+++ cpan/Text-TEI-Collate/lib/Text/TEI/Collate/Error.pm
@@ -3,9 +3,15 @@ package Text::TEI::Collate::Error;
 use strict;
 use warnings;
 use Moose;
+use overload '""' => \&_stringify, 'fallback' => 1;
 
 with qw/ Throwable::X /;
 use Throwable::X -all;
+
+sub _stringify {
+       my $self = shift;
+       return "Error: " . $self->ident . " // " . $self->message . "\n";
+}
 
 =head1 NAME
 
@@ -18,7 +24,7 @@ A basic exception class to throw around, as it were.
 =cut
 
 no Moose;
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 
 =head1 AUTHOR
 
